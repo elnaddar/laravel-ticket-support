e17b92d04a206311a5b96e597bcf42970a2cedbf
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 15:02:25 +0200

    feat: create ticket filter class.

---
+++ app/Http/Filters/V1/TicketFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Filters\V1;
+
+class TicketFilter extends QueryFilter
+{
+    public function status($value)
+    {
+        return $this->builder->where('status', $value);
+    }
+}
+
+//
