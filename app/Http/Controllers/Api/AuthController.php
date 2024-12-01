b4ae0f0b5aff274d6c9523afe8597ae57fd4f7b4
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 10:41:16 +0200

    feat: create api login logic

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\Api\LoginUserRequest;
 
 class AuthController extends Controller
@@ -12,6 +14,16 @@ class AuthController extends Controller
     use ApiResponses;
     function login(LoginUserRequest $request)
     {
-        return $this->ok($request->email);
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only("email", "password"))) {
+            return $this->error("Invalid Credentials!", 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
+
+        return $this->ok("Authenticated", [
+            "token" => $user->createToken("API token for " . $user->email)->plainTextToken
+        ]);
     }
 }
