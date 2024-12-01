b91e41e2e8dce7ee65118f644500242ccf9d61b6
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 16:28:46 +0200

    feat: add createdAt and updatedAt filters.

--- app/Http/Filters/V1/TicketFilter.php
+++ app/Http/Filters/V1/TicketFilter.php
@@ -15,6 +15,24 @@ class TicketFilter extends QueryFilter
         return $this->builder->whereLike("title", $likeStr);
     }
 
+    public function createdAt($value)
+    {
+        $dates = explode(',', $value);
+        if (count($dates) > 1) {
+            return $this->builder->whereBetween("created_at", $dates);
+        }
+        return $this->builder->whereDate("created_at", $value);
+    }
+
+    public function updatedAt($value)
+    {
+        $dates = explode(',', $value);
+        if (count($dates) > 1) {
+            return $this->builder->whereBetween("updated_at", $dates);
+        }
+        return $this->builder->whereDate("updated_at", $value);
+    }
+
     public function include($value)
     {
         return $this->builder->with($value);
