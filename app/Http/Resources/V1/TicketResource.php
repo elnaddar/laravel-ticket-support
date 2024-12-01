e9fbc25cda0f85d0edc3638bd5f98ba4e2767599
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 12:56:57 +0200

    add comment to help change wrapper.

--- app/Http/Resources/V1/TicketResource.php
+++ app/Http/Resources/V1/TicketResource.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TicketResource extends JsonResource
 {
+    // use this if you need to change response
+    // from {"data": [...]} to {"ticket": [...]}
+    // public static $wrap = "ticket";
+    
     /**
      * Transform the resource into an array.
      *
