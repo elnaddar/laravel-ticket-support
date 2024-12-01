7d240fb79c15d4825b536fdec2fd78031593ae0b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 10:24:11 +0200

    dev: create error response method, and success can now accept data.

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -4,16 +4,25 @@ namespace App\Traits;
 
 trait ApiResponses
 {
-    protected function ok($message)
+    protected function ok($message, $data = [])
     {
-        return $this->success($message, 200);
+        return $this->success($message, $data, 200);
     }
 
-    protected function success($message, $statusCode = 200)
+    protected function success($message, $data = [], $statusCode = 200)
     {
         return response()->json([
             "message" => $message,
-            "status" => $statusCode
+            "status" => $statusCode,
+            "data" => $data
+        ], $statusCode);
+    }
+
+    protected function error($message, $statusCode)
+    {
+        return response()->json([
+            "message" => $message,
+            "status" => $statusCode,
         ], $statusCode);
     }
 }
