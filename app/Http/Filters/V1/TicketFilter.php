bbba3415a85fde93346c92173c4bbd208f78cd58
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 16:23:45 +0200

    feat: added title filter that accepts queries.

--- app/Http/Filters/V1/TicketFilter.php
+++ app/Http/Filters/V1/TicketFilter.php
@@ -9,6 +9,12 @@ class TicketFilter extends QueryFilter
         return $this->builder->whereIn('status', explode(',', $values));
     }
 
+    public function title($value)
+    {
+        $likeStr = str_replace("*", "%", $value);
+        return $this->builder->whereLike("title", $likeStr);
+    }
+
     public function include($value)
     {
         return $this->builder->with($value);
