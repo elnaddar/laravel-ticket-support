edd480f9725c17ad3a18366c580198aa51b64ad0
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-02 17:47:53 +0200

    feat: add sortable attribute to QueryFilter.

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 abstract class QueryFilter
 {
     protected Builder $builder;
-
+    protected $sortable = [];
     public function __construct(protected Request $request) {}
 
     public function apply(Builder $builder)
@@ -40,13 +40,23 @@ abstract class QueryFilter
 
         foreach ($sortValues as $sortValue) {
             $sortDirection = "asc";
-            
+
             if ($sortValue[0] == '-') {
                 $sortDirection = "desc";
                 $sortValue = substr($sortValue, 1);
             }
 
-            $this->builder->orderBy($sortValue, $sortDirection);
+            $sortable = $this->sortable;
+
+            if (in_array($sortValue, $sortable)) {
+                $columnName = $sortValue;
+            } else if (array_key_exists($sortValue, $sortable)) {
+                $columnName = $sortable[$sortValue];
+            } else {
+                continue;
+            }
+
+            $this->builder->orderBy($columnName, $sortDirection);
         }
 
         return $this->builder;
