3b14d80ae2cee0f7082bbb38b9c7fbb90e5a01bb
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-15 21:04:14 +0200

    doc: add authentication section to the documentation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,6 +13,21 @@ use App\Permissions\V1\Abilities;
 class AuthController extends Controller
 {
     use ApiResponses;
+    /**
+    * Login
+    *
+    * Authenticates the user and returns the user's API token.
+    *
+    * @unauthenticated
+    * @group Authentication
+    * @response 200 {
+        "message": "Authenticated",
+        "status": 200,
+        "data": {
+            "token": "{YOUR_AUTH_KEY}"
+        }
+    }
+    */
     function login(LoginUserRequest $request)
     {
         $request->validated($request->all());
@@ -21,17 +36,25 @@ class AuthController extends Controller
             return $this->error("Invalid Credentials!", 401);
         }
 
-        $user = User::firstWhere('email', $request->email);
+        $user = User::firstWhere("email", $request->email);
 
         return $this->ok("Authenticated", [
             "token" => $user->createToken(
                 "API token for " . $user->email, // token name
                 Abilities::getAbilities($user), // abilities
                 now()->addMonth() // expiration date
-            )->plainTextToken
+            )->plainTextToken,
         ]);
     }
 
+    /**
+     * Logout
+     *
+     * Logouts the user and removes the API key.
+     *
+     * @group Authentication
+     * @response 200 {}
+     */
     function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
